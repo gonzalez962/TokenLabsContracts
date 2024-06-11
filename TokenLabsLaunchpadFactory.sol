6d4487293da07724f839979abe31339b35482203
gonzalez962 <gonzalez96scripts@gmail.com>
2024-06-10 23:53:49 -0600

    Update TokenLabsLaunchpadFactory.sol

--- TokenLabsLaunchpadFactory.sol
+++ TokenLabsLaunchpadFactory.sol
@@ -22,19 +22,9 @@ contract TokenLabsLaunchpadFactory is Ownable, ReentrancyGuard {
     address private _weth;
 
     struct SaleParams { 
-        address payable seller; 
-        ERC20 token; 
-        uint256 softcap; 
-        uint256 hardcap; 
-        uint256 startTime; 
-        uint256 endTime; 
-        uint256 tokensPerWei; 
-        uint256 tokensPerWeiListing; 
-        bool limitPerAccountEnabled; 
-        uint256 limitPerAccount; 
-        address pairingToken; 
-        uint256 referralRewardPercentage; 
-        uint256 rewardPool; 
+        address payable seller; ERC20 token; uint256 softcap; uint256 hardcap; uint256 startTime; uint256 endTime; 
+        uint256 tokensPerWei; uint256 tokensPerWeiListing; bool limitPerAccountEnabled; uint256 limitPerAccount; 
+        address pairingToken; uint256 referralRewardPercentage; uint256 rewardPool; 
     }
 
     constructor(address payable feeReceiver, IUniswapV2Router02 router, address weth) Ownable(msg.sender) {
@@ -58,10 +48,18 @@ contract TokenLabsLaunchpadFactory is Ownable, ReentrancyGuard {
         return address(newSale);
     }
 
-    function setFeeReceiver(address payable feeReceiver) external onlyOwner { _feeReceiver = feeReceiver; }
-    function setFeeAmount(uint256 newFeeAmount) external onlyOwner { _feeAmount = newFeeAmount; }
+    function setFeeReceiver(address payable feeReceiver) external onlyOwner {
+        _feeReceiver = feeReceiver;
+    }
+
+    function setFeeAmount(uint256 newFeeAmount) external onlyOwner {
+        _feeAmount = newFeeAmount;
+    }
+
     function getFeeReceiver() external view returns (address payable) { return _feeReceiver; }
+
     function getFeeAmount() external view returns (uint256) { return _feeAmount; }
+
     function getSales() public view returns (address[] memory) { return sales; }
 }
 
@@ -69,24 +67,12 @@ contract SaleContract is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     struct Sale { 
-        address payable seller; 
-        ERC20 token; 
-        uint256 softcap; 
-        uint256 hardcap; 
-        uint256 startTime; 
-        uint256 endTime; 
-        uint256 tokensPerWei; 
-        uint256 tokensPerWeiListing; 
-        uint256 collectedETH; 
-        bool limitPerAccountEnabled; 
-        uint256 limitPerAccount; 
-        uint256 referralRewardPercentage; 
-        uint256 rewardPool; 
+        address payable seller; ERC20 token; uint256 softcap; uint256 hardcap; uint256 startTime; uint256 endTime; 
+        uint256 tokensPerWei; uint256 tokensPerWeiListing; uint256 collectedETH; bool limitPerAccountEnabled; 
+        uint256 limitPerAccount; uint256 referralRewardPercentage; uint256 rewardPool; 
     }
 
-    struct AdditionalSaleDetails { 
-        address pairingToken; 
-    }
+    struct AdditionalSaleDetails { address pairingToken; }
 
     Sale public sale;
     AdditionalSaleDetails public additionalSaleDetails;
@@ -106,10 +92,18 @@ contract SaleContract is ReentrancyGuard {
         weth = _weth;
     }
 
-    modifier onlyAdmin() { require(admins[msg.sender] == true, "Caller is not authorized"); _; }
+    modifier onlyAdmin() {
+        require(admins[msg.sender] == true, "Caller is not authorized");
+        _;
+    }
 
-    function addAdmin(address newAdmin) public onlyAdmin { admins[newAdmin] = true; }
-    function removeAdmin(address adminToRemove) public onlyAdmin { admins[adminToRemove] = false; }
+    function addAdmin(address newAdmin) public onlyAdmin {
+        admins[newAdmin] = true;
+    }
+
+    function removeAdmin(address adminToRemove) public onlyAdmin {
+        admins[adminToRemove] = false;
+    }
 
     function buyTokens(uint256 erc20Amount, address referrer) public payable nonReentrant {
         require(block.timestamp >= sale.startTime && block.timestamp <= sale.endTime, "Sale is not ongoing");
@@ -135,10 +129,7 @@ contract SaleContract is ReentrancyGuard {
             }
         }
 
-        if (purchaseAmount + sale.collectedETH > sale.softcap && sale.collectedETH < sale.softcap) {
-            excessAmount += purchaseAmount + sale.collectedETH - sale.softcap;
-            purchaseAmount -= excessAmount;
-        } else if (purchaseAmount + sale.collectedETH > sale.hardcap) {
+        if (purchaseAmount + sale.collectedETH > sale.hardcap) {
             excessAmount += purchaseAmount + sale.collectedETH - sale.hardcap;
             purchaseAmount -= excessAmount;
         }
@@ -147,9 +138,13 @@ contract SaleContract is ReentrancyGuard {
         require(amountOfTokens > 0, "Not enough amount for tokens");
 
         tokenAmounts[msg.sender] += amountOfTokens;
+        
         if (excessAmount > 0 && isETH) payable(msg.sender).transfer(excessAmount);
 
-        sale.collectedETH = address(this).balance;
+        if (excessAmount > 0 && !isETH) IERC20(additionalSaleDetails.pairingToken).safeTransfer(msg.sender, excessAmount);
+
+        sale.collectedETH = isETH ? address(this).balance : IERC20(additionalSaleDetails.pairingToken).balanceOf(address(this));
+
         contributions[msg.sender] += purchaseAmount;
 
         if (referrer != address(0) && sale.rewardPool > 0) {
@@ -159,17 +154,16 @@ contract SaleContract is ReentrancyGuard {
             }
             referralRewards[referrer] += referralReward;
             sale.rewardPool -= referralReward;
-            
         }
     }
 
     function addLiquidityToDEX(uint256 tokenAmount, uint256 ethAmount) private {
         IERC20(address(sale.token)).approve(address(dexRouter), tokenAmount);
         if (additionalSaleDetails.pairingToken == address(0)) {
-            dexRouter.addLiquidityETH{value: ethAmount}( address(sale.token), tokenAmount, 0, 0, address(0), block.timestamp );
+            dexRouter.addLiquidityETH{value: ethAmount}(address(sale.token), tokenAmount, 0, 0, address(0), block.timestamp);
         } else {
             IERC20(additionalSaleDetails.pairingToken).approve(address(dexRouter), ethAmount);
-            dexRouter.addLiquidity( address(sale.token), additionalSaleDetails.pairingToken, tokenAmount, ethAmount, 0, 0, address(0), block.timestamp );
+            dexRouter.addLiquidity(address(sale.token), additionalSaleDetails.pairingToken, tokenAmount, ethAmount, 0, 0, address(0), block.timestamp);
         }
     }
 
@@ -178,21 +172,30 @@ contract SaleContract is ReentrancyGuard {
         require(block.timestamp > sale.endTime || sale.collectedETH >= sale.hardcap, "Sale end conditions not met");
         if (sale.collectedETH < sale.softcap) return;
 
-        uint256 liquidityETH = sale.collectedETH;
-        uint256 excessETH = (sale.collectedETH > sale.hardcap) ? sale.collectedETH - sale.hardcap : 0;
-        liquidityETH = (excessETH > 0) ? sale.hardcap : liquidityETH;
+        uint256 liquidityETH = sale.collectedETH > sale.hardcap ? sale.hardcap : sale.collectedETH;
+        uint256 excessETH = sale.collectedETH > sale.hardcap ? sale.collectedETH - sale.hardcap : 0;
 
         if (sale.collectedETH >= sale.softcap && sale.collectedETH < sale.hardcap) {
-            uint256 remainingEth = (sale.hardcap - sale.collectedETH);
-            uint256 remainingTokens = (remainingEth * sale.tokensPerWeiListing);
+            uint256 remainingEth = sale.hardcap > sale.collectedETH ? sale.hardcap - sale.collectedETH : 0;
+            uint256 remainingTokens = (remainingEth * sale.tokensPerWeiListing) + (remainingEth * sale.tokensPerWei);
             ERC20Burnable token = ERC20Burnable(address(sale.token));
-            token.burn(remainingTokens);
-            ERC20Burnable(address(sale.token)).burn(sale.rewardPool);
+            if (remainingTokens > 0) {
+                token.burn(remainingTokens);
+            }
+            if (sale.rewardPool > 0) {
+                token.burn(sale.rewardPool);
+            }
         }
 
-        if (excessETH > 0) sale.seller.transfer(excessETH);
+        if (excessETH > 0) {
+            if (additionalSaleDetails.pairingToken == address(0)) {
+                sale.seller.transfer(excessETH);
+            } else {
+                IERC20(additionalSaleDetails.pairingToken).safeTransfer(sale.seller, excessETH);
+            }
+        }
 
-        uint256 liquidityToken = (liquidityETH) * sale.tokensPerWeiListing;
+        uint256 liquidityToken = liquidityETH * sale.tokensPerWeiListing;
         if (additionalSaleDetails.pairingToken == address(0)) {
             addLiquidityToDEX(liquidityToken, liquidityETH);
         } else {
@@ -204,13 +207,24 @@ contract SaleContract is ReentrancyGuard {
         isListed = true;
     }
 
+
     function claim() external nonReentrant {
         require(block.timestamp > sale.endTime, "Sale has not ended");
         if (sale.collectedETH < sale.softcap) {
+            uint256 remainingTokens = IERC20(address(sale.token)).balanceOf(address(this));
             uint256 ethAmount = contributions[msg.sender];
-            require(ethAmount > 0, "No amount available to claim");
-            contributions[msg.sender] = 0;
-            payable(msg.sender).transfer(ethAmount);
+
+            if (sale.seller == msg.sender && remainingTokens > 0) {
+                IERC20(address(sale.token)).safeTransfer(msg.sender, remainingTokens);
+                if(ethAmount > 0){
+                    contributions[msg.sender] = 0;
+                    payable(msg.sender).transfer(ethAmount);
+                }
+            } else {
+                require(ethAmount > 0, "No amount available to claim");
+                contributions[msg.sender] = 0;
+                payable(msg.sender).transfer(ethAmount);
+            }
         } else {
             uint256 tokens = tokenAmounts[msg.sender];
             uint256 referralReward = referralRewards[msg.sender];
@@ -229,19 +243,34 @@ contract SaleContract is ReentrancyGuard {
     }
 
     function balanceOf(address account) public view returns (uint256) { return tokenAmounts[account]; }
+
     function getTokenBalance() public view returns (uint256) { return sale.token.balanceOf(address(this)); }
+
     function getCollectedETH() public view returns (uint256) { return sale.collectedETH; }
+
     function getSoftcap() public view returns (uint256) { return sale.softcap; }
+
     function getHardcap() public view returns (uint256) { return sale.hardcap; }
+
     function getStartTime() public view returns (uint256) { return sale.startTime; }
+
     function getEndTime() public view returns (uint256) { return sale.endTime; }
+
     function getExpectedLiquidityETH() public view returns (uint256) { return sale.collectedETH; }
+
     function getContractETHBalance() public view returns (uint256) { return address(this).balance; }
+
     function getSellerAddress() public view returns (address payable) { return sale.seller; }
+
     function getLiquidityETH() public view returns (uint256) { return sale.collectedETH; }
-    function getLiquidityTokenAmount() public view returns (uint256) { return (sale.collectedETH == 0) ? 0 : ((sale.collectedETH > sale.hardcap ? sale.hardcap : sale.collectedETH)) * sale.tokensPerWeiListing; }
+
+    function getLiquidityTokenAmount() public view returns (uint256) { return (sale.collectedETH == 0) ? 0 : ((sale.collectedETH > sale.hardcap ? sale.hardcap : sale.collectedETH) * sale.tokensPerWeiListing); }
+
     function getTokenContract() public view returns (IERC20) { return sale.token; }
+
     function getTokensPerWei() public view returns (uint256) { return sale.tokensPerWei; }
+
     function getTokensPerWeiListing() public view returns (uint256) { return sale.tokensPerWeiListing; }
+
     function getContributions(address user) public view returns (uint256) { return contributions[user]; }
 }
